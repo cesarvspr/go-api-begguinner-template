aa12ad888eeff5a72021ca23d8cc2156730ad707
cesarvspr <vinicius_spr@hotmail.com>
2021-05-30 03:12:35 -0400

    v3.4

--- images-server/files_sdk/storage.go
+++ images-server/files_sdk/storage.go
@@ -1,9 +1,4 @@
 package files_sdk
 
-import "io"
-
 // Storage defines the behavior for file operations
 // Implementations may be of the time local disk, or cloud storage, etc
-type Storage interface {
-	Save(path string, file io.Reader) error 
-}
